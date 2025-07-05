0f90e9311ffb94277c5271481e73435fc52c6e93
Shakib <shakibhossain2020124@gmail.com>
2025-07-05 20:31:38 +0600

    state + rendering with state

--- react-2/react2/src/pages/Home.jsx
+++ react-2/react2/src/pages/Home.jsx
@@ -1,21 +1,43 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 function Home()
 {
+
+    const [searchQuery, setSearchQuery] = useState("");
+
     const movies = [
         {id: 1,title:"john wick",release_date:"2-7-25"},
         {id: 2,title:"john wick2",release_date:"2-7-25"},
         {id: 3,title:"john wick3",release_date:"2-7-25"}
     ];
 
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+        //setSearchQuery("previous input")
+    }
+
     return(
         <>
         <div className="home">
-            <div>
+            <form className="search-form" onSubmit={handleSearch}>
+                <input
+                type="text"
+                placeholder = "search for movies..."
+                className="search-input"
+                value={searchQuery}
+                onChange={(e)=> setSearchQuery(e.target.value)}
+                />
+                <button type="submit" className="search-button">enter</button>
+            </form>
+
+            <div className="movies-grid">
                 {
                     movies.map(
                         (movie) => (
-                            <MovieCard movie={movie} key={movie.id}></MovieCard>
+                            movie.title.toLowerCase().startsWith(searchQuery) &&
+                            (<MovieCard movie={movie} key={movie.id}></MovieCard>)
                         )
                     )
                 }
